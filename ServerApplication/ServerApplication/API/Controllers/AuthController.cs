e453f217972b7a4d38da8f11db3ba01c30196272
Bödő Lajos <ifj.bodo.lajos@gmail.com>
2020-11-25 00:39:41 +0100

    Adding AuthController.cs

---
+++ ServerApplication/ServerApplication/API/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ServerApplication.API.DTOs.Auth;
+using ServerApplication.BLL.Models.Auth;
+using ServerApplication.BLL.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApplication.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        IAuthService authService;
+        IMapper mapper;
+
+        public AuthController(IAuthService authService, IMapper mapper)
+        {
+            this.authService = authService;
+            this.mapper = mapper;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthData>> PostAsync([FromBody]LoginData model)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            try
+            {
+                var id = await authService.LogInUser(mapper.Map<Login>(model));
+                return authService.GetAuthdata(id);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { error = e.Message });
+            }
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthData>> PostAsync([FromBody]RegistrationData model)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            try
+            {
+                var id = await authService.CreateNewUser(mapper.Map<Registration>(model));
+
+                return authService.GetAuthdata(id);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { error = e.Message });
+            }
+        }
+    }
+}
