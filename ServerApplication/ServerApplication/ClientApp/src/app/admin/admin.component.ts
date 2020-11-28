2584f065d2d1619b75f228d4ab72d54f8b8026a5
Kósa Szilárd <kszilard777@gmail.com>
2020-11-28 18:01:15 +0100

    Admin user table started, fontawesome added

--- ServerApplication/ServerApplication/ClientApp/src/app/admin/admin.component.ts
+++ ServerApplication/ServerApplication/ClientApp/src/app/admin/admin.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "../http.service";
+import { User } from "../models/user";
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  selector: "app-admin",
+  templateUrl: "./admin.component.html",
+  styleUrls: ["./admin.component.css"],
 })
 export class AdminComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private httpService: HttpService) {}
 
   ngOnInit() {
+    this.httpService.getAllUser().subscribe((users) => (this.users = users));
   }
-
 }
